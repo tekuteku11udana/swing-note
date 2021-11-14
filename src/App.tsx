638eaa32c2d5fdce11a7585cae2d941e610bb57e
tekuteku11udana <tekuteku11udana@gmail.com>
2021-11-14 21:45:28 +0900

    ?

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from './components/TodoList';
+
+// import './App.css';
+
+function App() {
+  return (
+    <p>Hello</p>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,7 @@
+const TodoList = () => {
+    return (
+        <p>add list</p>
+    )
+}
+
+export default TodoList
